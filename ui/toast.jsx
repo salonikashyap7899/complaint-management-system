11c8277e25c1715165973f68e870a71ebade3c8e
Saloni Kashyap <124189127+salonikashyap7899@users.noreply.github.com>
2026-01-01 11:02:46 +0530

    resolve error

--- ui/toast.jsx
+++ ui/toast.jsx
@@ -4,8 +4,7 @@ import * as ToastPrimitives from "@radix-ui/react-toast"
 import { cva } from "class-variance-authority";
 import { X } from "lucide-react"
 
-import { cn } from "@/lib/utils"
-
+import { cn } from "../lib/utils"
 const ToastProvider = ToastPrimitives.Provider
 
 const ToastViewport = React.forwardRef(({ className, ...props }, ref) => (
